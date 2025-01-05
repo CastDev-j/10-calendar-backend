d2fef72dc686ade754da5e612984ce885dbbd542
AndDev <23031429@itcelaya.edu.mx>
2025-01-04 18:16:30 -0600

    update default values in env.js for appPort, dbCnn, and secretJwtSeed

--- config/env.js
+++ config/env.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 
-const appPort = process.env.PORT || 6969;
-const dbCnn = process.env.DB_CNN;
-const secretJwtSeed = process.env.SECRET_JWT_SEED;
+const appPort = process.env.PORT || "6969";
+const dbCnn = process.env.DB_CNN || "mongodb+srv://castdev:jugar2017%40CJ@cluster0.w9bfa.mongodb.net/mern_calendar" ;
+const secretJwtSeed = process.env.SECRET_JWT_SEED || "cepillin eres un naco y un estupido";
 
 module.exports = {
     appPort,
